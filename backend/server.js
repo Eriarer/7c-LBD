1ff5b1c65d25be58f5a08d28177eda3d5a92c6fb
eriarer <eriarer_messiah+git@hotmail.com>
2024-11-03 13:23:49 -0600

    chore(imports): removing unused imports

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 import app from './app.js'
-import { appConfig, dbConfig } from './config.js'
+import { appConfig } from './config.js'
 
 async function init(appConfig) {
   try {
