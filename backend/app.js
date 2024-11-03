8d367243601ed8c8168f3b82f97edf34af9efd8f
eriarer <eriarer_messiah+git@hotmail.com>
2024-11-03 00:11:37 -0600

    feat: update app.js to use express middleware and add usuario routes
    Co-authored-by: Leonardo <Laxlev@users.noreply.github.com>

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import bodyParser from 'body-parser'
+import usuarioRoutes from './routes/usuario.routes.js'
 
 const app = express()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/glem', usuarioRoutes)
 
 export default app
