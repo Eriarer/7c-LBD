b0d921595727e6925b24363a8b38027836fa97ec
eriarer <eriarer_messiah+git@hotmail.com>
2024-11-17 23:14:11 -0600

    feat(db): simplify database connection using dbConfig

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,11 +1,4 @@
 import { createPool } from "mysql2/promise"
+import { dbConfig } from "../config.js"
 
-export const pool = createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  dateStrings: true,
-  timezone: '-06:00'
-})
+export const pool = createPool(dbConfig)
