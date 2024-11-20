a5f8d6e460dba091cfe8103aa560822e24c3f5d4
Eriarer <eriarer_melgoza+git@hotmail.com>
2024-11-20 11:54:03 -0600

    feat(routes): update usuario routes for cleaner endpoints and add prettier configuration

--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -1,19 +1,27 @@
 import express from 'express'
 import { validateSchema } from '../middleware/validateSchema.middleware.js'
 import { addUsuarioSchema } from '../schema/usuario.schema.js'
-import { addUsuario, deleteUsuario, deleteUsuarios, updateUsuario, getUsuarios, getUsuariosFiltered, getUsuarioById } from "../controllers/usuario.controller.js"
+import {
+  addUsuario,
+  deleteUsuario,
+  deleteUsuarios,
+  updateUsuario,
+  getUsuarios,
+  getUsuariosFiltered,
+  getUsuarioById
+} from '../controllers/usuario.controller.js'
 
 const router = express.Router()
 
-router.post('/usuario', validateSchema(addUsuarioSchema), addUsuario)
+router.post('/', validateSchema(addUsuarioSchema), addUsuario)
 
-router.delete('/usuario/:id', deleteUsuario)
-router.delete('/usuarios', deleteUsuarios)
+router.delete('/:id', deleteUsuario)
+router.delete('/', deleteUsuarios)
 
-router.patch('/usuario/:id', updateUsuario)
+router.patch('/:id', updateUsuario)
 
-router.get('/usuarios', getUsuarios)
-router.get('/usuarios/filtered', getUsuariosFiltered)
-router.get('/usuario/:id', getUsuarioById)
+router.get('/find', getUsuarios)
+router.get('/query', getUsuariosFiltered)
+router.get('/:id', getUsuarioById)
 
 export default router
