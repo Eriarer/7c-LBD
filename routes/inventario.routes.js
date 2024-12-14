e42b4e572e507036861637f86d97bc9d2b144f0c
eriarer <eriarer_messiah+git@hotmail.com>
2024-12-14 16:27:25 -0600

    style(routes): format import statements for consistency in inventario.routes.js

--- routes/inventario.routes.js
+++ routes/inventario.routes.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import { validateSchema } from '../middleware/validateSchema.middleware.js'
 import {
-    addMaterial,
-    getMaterial,
-    getMaterialById,
-    deleteMaterial,
+  addMaterial,
+  getMaterial,
+  getMaterialById,
+  deleteMaterial
 } from '../controllers/material.controller.js'
 
 const router = express.Router()
