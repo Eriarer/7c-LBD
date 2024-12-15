4083d61d071239e83abb493503b9dc2eed3d9ee2
eriarer <eriarer_messiah+git@hotmail.com>
2024-12-15 10:04:26 -0600

    feat(schema): simplify materiales validation in addPrestamoSchema; ensure array structure with improved error messaging

--- schema/prestamo.schema.js
+++ schema/prestamo.schema.js
@@ -34,19 +34,7 @@ export const addPrestamoSchema = z.object({
     .default('P') // Valor predeterminado 'P'
     .optional(),
   materiales: z
-    .array()
-    .items(
-      z.object({
-        idunidad: z
-          .number({ message: 'El Id de unidad es requerido' })
-          .int({ message: 'El Id de unidad debe ser un número entero' })
-          .positive({ message: 'El Id de unidad debe ser mayor a 0' }),
-        cantidad: z
-          .number({ message: 'La cantidad es requerida' })
-          .int({ message: 'La cantidad debe ser un número entero' })
-          .positive({ message: 'La cantidad debe ser mayor a 0' })
-      })
-    )
+    .array({ message: 'Los materiales deben ser un arreglo de objetos' })
     .optional()
 })
 
@@ -89,7 +77,7 @@ export const updatePrestamoSchema = z
       .string()
       .length(1, { message: 'El estado debe ser un carácter' })
       .regex(/^[PCADF]$/, { message: 'El estado debe ser P, C, A, D o F' })
-      .default('P') // Valor predeterminado 'P'
+      .default('P')
       .optional()
   })
   .partial()
